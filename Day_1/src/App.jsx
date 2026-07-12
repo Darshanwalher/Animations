dfec170335746f149b2bf706cd3a56a69721ce2c
Darshanwalher <darshanwalher21@gmail.com>
2026-07-12 12:58:58 +0530

    Update animation duration and box height for improved effect

--- Day_1/src/App.jsx
+++ Day_1/src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const App = () => {
   const box = document.querySelector('.box');
 
-  gsap.to(box, { x: 300, duration: 10, repeat: -1, yoyo: true });
+  gsap.from(box, { delay: 0.6, x: 300, duration: 2, repeat: -1, yoyo: true });
 }
 
 export default App
