cb2035bb5ecf6b52c9c57c55c29b4b0227264c97
Darshanwalher <darshanwalher21@gmail.com>
2026-07-12 13:06:27 +0530

    Refactor animation to use fromTo for smoother transitions

--- Day_1/src/App.jsx
+++ Day_1/src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const App = () => {
   const box = document.querySelector('.box');
 
-  gsap.from(box, { delay: 0.6, x: 300, duration: 2, repeat: -1, yoyo: true });
+  gsap.fromTo(box, { delay: 0.6, x: 300, duration: 2, repeat: -1, yoyo: true },{ x: 0, duration: 2, repeat: -1, yoyo: true });
 }
 
 export default App
