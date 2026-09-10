28ad58e92e13c198c063416a0f0aa2a67358d7e6
Darshanwalher <darshanwalher21@gmail.com>
2026-09-10 23:24:07 +0530

    Update animation duration for improved visual effect

--- Day_1/src/App.jsx
+++ Day_1/src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const App = () => {
   const box = document.querySelector('.box');
 
-  gsap.fromTo(box, { delay: 0.6, x: 500, duration: 2, repeat: -1, yoyo: true },{ x: 0, duration: 2, repeat: -1, yoyo: true });
+  gsap.fromTo(box, { delay: 0.6, x: 500, duration: 2, repeat: -1, yoyo: true },{ x: 0, duration: 5, repeat: -1, yoyo: true });
 }
 
 export default App
