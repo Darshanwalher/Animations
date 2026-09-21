f2ef8b2c137306f240059763061ee4763a1e588e
Darshanwalher <darshanwalher21@gmail.com>
2026-09-21 22:06:35 +0530

    Refactor box animation settings and add console logs for start and completion events

--- Day_2/src/main.js
+++ Day_2/src/main.js
@@ -1,15 +1,19 @@
 import { gsap } from "gsap";
 import "./index.css";
 
-gsap.set('.box',{
-    overflow: 'hidden',
-    x: -300,
-})
+
 
 gsap.to('.box',{
-    x: 1550,
+    x: 500,
     duration: 3,
     delay: 0.2,
     ease: "expo.inOut",
-    repeat: -1,
+    onStart: () => {
+        console.log("animation started!...");
+        
+    },
+    onComplete: () => {
+        console.log("animation completed!...");
+        
+    }
 })
