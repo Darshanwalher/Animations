cea722bff2255615eb037cab5c84b362749c2e7a
Darshanwalher <darshanwalher21@gmail.com>
2026-09-22 15:22:56 +0530

    Initialize Day 3 project with Vite, React, and GSAP; add basic structure and styles

---
+++ Day_3/src/main.js
@@ -0,0 +1,28 @@
+import gsap from "gsap";
+import "./index.css"
+
+
+
+
+
+const tl = gsap.timeline();
+
+tl.to(".box1",{
+    x: 1200,
+    duration: 1.3,
+    ease: "power4.out",
+    delay: 0.6
+}).to(".box2",{
+    x: 1200,
+    duration: 1.3,
+    ease: "power4.out",
+}).to(".box3",{
+    x: 1200,
+    duration: 1.3,
+    ease: "power4.out",
+}).to(".box4",{
+    x: 1200,
+    duration: 1.3,
+    ease: "power4.out",
+
+})
